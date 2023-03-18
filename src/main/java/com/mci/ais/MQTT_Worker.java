e21dbf4ff4e4a6d23575233a8aed5aa1b417c2ca
Lukas Prenner <prenner.products@gmail.com>
2023-03-18 12:22:17 +0100

    finito Worker MQTT without Multithreading

--- src/main/java/com/mci/ais/MQTT_Worker.java
+++ src/main/java/com/mci/ais/MQTT_Worker.java
@@ -2,20 +2,19 @@ package com.mci.ais;
 
 
 import org.eclipse.paho.client.mqttv3.*;
-
-import javax.security.auth.callback.Callback;
-import java.util.Arrays;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MQTT_Worker implements MqttCallback {
 
-    private MqttClient mqtt;
-    private String workerId = UUID.randomUUID().toString();
-    private boolean shutdownflag = false;
+    // Class Variables
+    private final MqttClient mqtt;
+    private final String workerId = UUID.randomUUID().toString();
+    private boolean allDartsRecived = false;
+    //private static final int THREAD_COUNT = 16;
 
-    MQTT_Worker worker;
 
+    // Constructor for MQTT_Worker
     public MQTT_Worker(String mqtturl) throws MqttException {
         mqtt = new MqttClient(mqtturl, workerId);
     }
@@ -27,31 +26,30 @@ public class MQTT_Worker implements MqttCallback {
 
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
-        System.out.println("New message!");
+        System.out.println("New message received!");
         String [] topicparts = topic.split("/");
         if (topicparts[1].equals(workerId)){
-            System.out.println("Topicparts[1] = " + topicparts[1]);
+            //System.out.println("Topicparts[1] = " + topicparts[1]);
             // message signature=  [OK/NOK]: [darts, null]
             String [] msgparts = message.toString().split(":");
             if(msgparts[0].equals("OK")){
                 System.out.println("Worker gets OK stauts");
                 if (msgparts.length == 2){
                     long darts = Long.parseLong(msgparts[1]);
-                    System.out.println("Worker " + workerId + " recived " + msgparts[1] + " darts");
+                    System.out.println("Worker " + workerId + " received " + msgparts[1] + " darts");
                     // Throw darts and send MQTT message with the result to the coordinator
                     throwDartsAndSendMessage(darts);
                 }
-
             } else if (msgparts[0].equals("NOK")) {
-                System.out.println("Worker gets Status NOK! no more Darts to throw");
-                shutdownflag = true;
+                System.out.println("Worker gets Status NOK! All darts recived");
+                allDartsRecived = true;
             }else {
                 System.err.println("ERROR: unknown message-type");
             }
         }
         else if (topic.equals("worker/broadcast")){
             System.out.println("Broadcast arrived!");
-            // Nachricht an alle Worker
+            // Nachricht an alle Worker habe nur einen Worker implementiert
         }else {
             System.err.println("ERROR: no valid Message arrived!");
             System.err.println(message.toString());
@@ -80,12 +78,11 @@ public class MQTT_Worker implements MqttCallback {
     }
 
     public void work() throws InterruptedException, MqttException {
-        while (!shutdownflag){
+        while (!allDartsRecived){
             MqttMessage message = new MqttMessage();
             mqtt.publish("coordinator/request/" + workerId, message );
             Thread.sleep(500);
         }
-        disconnect();
     }
 
 
@@ -102,7 +99,7 @@ public class MQTT_Worker implements MqttCallback {
         }
         // send message
         String msg;
-        msg = String.valueOf(darts) + ":" +  String.valueOf(hits) ;
+        msg = darts + ":" + hits;
         MqttMessage sendMessage = new MqttMessage(msg.getBytes());
         mqtt.publish("coordinator/result", sendMessage);
         System.out.println("Worker sends result to Coordinator! Result: " + msg);
@@ -114,11 +111,12 @@ public class MQTT_Worker implements MqttCallback {
 
 
     public static void main(String[] args) throws MqttException, InterruptedException {
+        // Ohne MultiThreading
         MQTT_Worker worker1 = new MQTT_Worker("tcp://localhost:1883");
         worker1.connect();
         worker1.work();
         worker1.disconnect();
-
+        System.out.println("Finished!!!");
     }
 
 }
